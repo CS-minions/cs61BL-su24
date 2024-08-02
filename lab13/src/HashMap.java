b744cfc2f60d80e2fa8f1a153b1c8fca610be3f9
帅帅 <130915343+CS-minions@users.noreply.github.com>
2024-08-02 18:52:16 +0800

    lab 13 already done

--- lab13/src/HashMap.java
+++ lab13/src/HashMap.java
@@ -1,23 +1,32 @@
-public class HashMap {
+import java.util.Map;
+
+public class HashMap<K, V> implements Map61BL<K, V> {
 
     /* TODO: Instance variables here */
 
+    Entry<K, V>[] arr;
+    int size;
+
     /* TODO: Constructors here */
+    HashMap() {
+        arr = new Entry[26];
+        size = 0;
+    }
 
     /* TODO: Interface methods here */
 
-    private static class Entry {
+    private static class Entry<K, V> {
 
-        private String key;
-        private String value;
+        private K key;
+        private V value;
 
-        Entry(String key, String value) {
+        Entry(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
         /* Returns true if this key matches with the OTHER's key. */
-        public boolean keyEquals(Entry other) {
+        public boolean keyEquals(Entry<K, V> other) {
             return key.equals(other.key);
         }
 
@@ -25,13 +34,17 @@ public class HashMap {
         @Override
         public boolean equals(Object other) {
             return (other instanceof Entry
-                    && key.equals(((Entry) other).key)
-                    && value.equals(((Entry) other).value));
+                    && key.equals(((Entry<K, V>) other).key)
+                    && value.equals(((Entry<K, V>) other).value));
         }
 
         @Override
         public int hashCode() {
             return super.hashCode();
         }
+    }
+    @Override
+    public boolean containsKey(K key) {
+
     }
 }
