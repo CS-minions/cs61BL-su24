4967e05367c0692db616314497461ba7fcfe36ec
帅帅 <130915343+CS-minions@users.noreply.github.com>
2024-08-03 16:49:52 +0800

    lab14 heap and PQ

--- lab14/src/MinHeap.java
+++ lab14/src/MinHeap.java
@@ -69,61 +69,96 @@ public class MinHeap<E extends Comparable<E>> {
 
     /* Returns the index of the left child of the element at index INDEX. */
     private int getLeftOf(int index) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return index * 2;
     }
 
     /* Returns the index of the right child of the element at index INDEX. */
     private int getRightOf(int index) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return index * 2 + 1;
     }
 
     /* Returns the index of the parent of the element at index INDEX. */
     private int getParentOf(int index) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return index / 2;
     }
 
     /* Returns the index of the smaller element. At least one index has a
        non-null element. If the elements are equal, return either index. */
     private int min(int index1, int index2) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (getElement(index1) == null) {
+            return index2;
+        }
+        if (getElement(index2) == null) {
+            return index1;
+        }
+        if (getElement(index2).compareTo(getElement(index1)) < 0) {
+            return index2;
+        } else {
+            return index1;
+        }
     }
 
     /* Returns but does not remove the smallest element in the MinHeap. */
     public E findMin() {
         // TODO: YOUR CODE HERE
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        return getElement(1);
     }
 
     /* Bubbles up the element currently at index INDEX. */
     private void bubbleUp(int index) {
-        // TODO: YOUR CODE HERE
+        while (index > 1 && getElement(index).compareTo(getElement(getParentOf(index))) < 0) {
+            swap(index,getParentOf(index));
+            bubbleUp(getParentOf(index));
+        }
     }
 
     /* Bubbles down the element currently at index INDEX. */
     private void bubbleDown(int index) {
         // TODO: YOUR CODE HERE
+        while ( 2 * index <= size &&
+                getElement(index).compareTo(getElement(min(getLeftOf(index), getRightOf(index)))) > 0) {
+            int swapindex = min(getLeftOf(index), getRightOf(index));
+            swap(index, swapindex);
+            bubbleDown(swapindex);
+        }
+
     }
 
     /* Returns the number of elements in the MinHeap. */
     public int size() {
         // TODO: YOUR CODE HERE
-        return 0;
+        return size;
     }
 
     /* Inserts ELEMENT into the MinHeap. If ELEMENT is already in the MinHeap,
        throw an IllegalArgumentException.*/
     public void insert(E element) {
-        // TODO: YOUR CODE HERE
+        if (contains(element)) {
+            throw new IllegalArgumentException();
+        } else {
+            size ++;
+            setElement(size, element);
+            bubbleUp(size);
+        }
     }
 
     /* Returns and removes the smallest element in the MinHeap, or null if there are none. */
     public E removeMin() {
         // TODO: YOUR CODE HERE
-        return null;
+        E min = findMin();
+        swap(1, size);
+        contents.remove(size);
+        size--;
+        bubbleDown(1);
+
+
+        return min;
     }
 
     /* Replaces and updates the position of ELEMENT inside the MinHeap, which
--- lab14/test/MinHeapTest.java
+++ lab14/test/MinHeapTest.java
@@ -1,11 +1,75 @@
+import org.junit.Before;
 import org.junit.Test;
-
-import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.*;
 
 public class MinHeapTest {
+    private MinHeap<Integer> heap;
+
+    @Before
+    public void setUp() {
+        heap = new MinHeap<>();
+    }
 
     @Test
-    public void test1() {
+    public void testHeapInitialization() {
+        assertTrue("Heap should be initialized empty", heap.size() == 0);
+        assertNull("Min of an empty heap should be null", heap.findMin());
+    }
 
+    @Test
+    public void testInsertAndRemoveSingleElement() {
+        heap.insert(10);
+        assertEquals("Heap size should be 1 after one insert", 1, heap.size());
+        assertEquals("Min should be the only element inserted", Integer.valueOf(10), heap.findMin());
+        assertEquals("Remove min should return the only element", Integer.valueOf(10), heap.removeMin());
+        assertTrue("Heap should be empty after removal", heap.size() == 0);
+    }
+
+    @Test
+    public void testInsertMultipleElements() {
+        int[] elements = {5, 2, 9, 1, 7};
+        for (int element : elements) {
+            heap.insert(element);
+        }
+        assertEquals("Correct min after multiple inserts", Integer.valueOf(1), heap.findMin());
+    }
+
+    @Test
+    public void testSequentialRemoveMin() {
+        int[] elements = {5, 3, 8, 1, 6};
+        for (int element : elements) {
+            heap.insert(element);
+        }
+        assertEquals("First min should be 1", Integer.valueOf(1), heap.removeMin());
+        assertEquals("Next min should be 3", Integer.valueOf(3), heap.removeMin());
+        assertEquals("Next min should be 5", Integer.valueOf(5), heap.removeMin());
+        assertEquals("Next min should be 6", Integer.valueOf(6), heap.removeMin());
+        assertEquals("Last min should be 8", Integer.valueOf(8), heap.removeMin());
+        assertTrue("Heap should be empty after all removals", heap.size() == 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInsertDuplicateElement() {
+        heap.insert(4);
+        heap.insert(4); // This should throw IllegalArgumentException
+    }
+
+    @Test
+    public void testRemoveMinOnEmptyHeap() {
+        assertNull("Removing min from an empty heap should return null", heap.removeMin());
+    }
+
+    @Test
+    public void testHeapPropertyAfterRandomInsertsAndRemoves() {
+        int[] elements = {10, 4, 15, 1, 3, 9, 20, 15, 8, 2};
+        for (int element : elements) {
+            heap.insert(element);
+        }
+        Integer prevMin = heap.removeMin(); // Should be 1
+        while (heap.size() > 0) {
+            Integer currentMin = heap.removeMin();
+            assertTrue("Each removed element should be greater than the last", prevMin <= currentMin);
+            prevMin = currentMin;
+        }
     }
 }
